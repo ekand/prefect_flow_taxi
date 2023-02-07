a00628717825dad73477803abd31dfd78eb0a858
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-07 15:03:43 -0600

    use index=False in df.to_gbq

--- etl_gcs_to_bq.py
+++ etl_gcs_to_bq.py
@@ -19,7 +19,7 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
 @task()
 def extract(path: Path) -> pd.DataFrame:
     """Data cleaning example"""
-    df = pd.read_parquet(path, index=False)
+    df = pd.read_parquet(path)
     # print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
     # df["passenger_count"].fillna(0, inplace=True)
     # print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
@@ -38,6 +38,7 @@ def write_bq(df: pd.DataFrame) -> None:
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
         if_exists="append",
+        index=False,
     )
 
 
