bfe05182a57418109dbde19b7436273ddf819a4c
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-07 14:06:24 -0600

    initial commit: add files

---
+++ etl_gcs_to_bq.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+
+
+@task(retries=0, log_prints=True)
+def extract_from_gcs(color: str, year: int, month: int) -> Path:
+    """Download trip data from GCS"""
+    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
+    gcs_block = GcsBucket.load("my-gcs-bucket")
+    print(f'gcs_path 1: {gcs_path}')
+    gcs_block.get_directory(from_path=gcs_path, local_path='data')
+    print(f'gcs_path 2: {gcs_path}')
+    return Path(gcs_path)
+
+
+@task()
+def extract(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    df = pd.read_parquet(path)
+    # print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    # df["passenger_count"].fillna(0, inplace=True)
+    # print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+    return df
+
+
+@task()
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BiqQuery"""
+
+    gcp_credentials_block = GcpCredentials.load("my-gcs-creds")
+
+    df.to_gbq(
+        destination_table="my_dataset.yellow_rides",
+        project_id="dtc-de-376923",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append",
+    )
+
+
+@flow(log_prints=True)
+def etl_gcs_to_bq(month: int, year: int, color: str):
+    """Main ETL flow to load data into Big Query"""
+    path = extract_from_gcs(color, year, month)
+    print(f'path: {path}')
+    df = extract(path)
+    print(f'Number of rows: {len(df)}')
+    write_bq(df)
+
+@flow()
+def parent_etl_flow(months: list[int], year: int, color: str):
+    for month in months:
+        etl_gcs_to_bq(month=month, year=year, color=color)
+
+if __name__ == "__main__":
+    months=[1]
+    year=2021
+    color='green'
+
+    parent_etl_flow(months=months, year=year, color=color)
---
+++ etl_web_to_gcs.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+    # if randint(0, 1) > 0:
+    #     raise Exception
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("my-gcs-bucket")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+
+@flow()
+def etl_web_to_gcs(color: str, year: int, month: int) -> None:
+    """The main ETL function"""
+
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+@flow()
+def etl_parent_flow(color: str, year: int, months: list[int]) -> None:
+    for month in months:
+        etl_web_to_gcs(color=color, year=year, month=month)
+
+if __name__ == "__main__":
+    color = "yellow"
+    year = 2019
+    months = [3, 4, 5]
+    etl_parent_flow(color=color, year=year, months=months)
+    
